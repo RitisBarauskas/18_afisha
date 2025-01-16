20847f030fc83cbff39265fcbd0404432970f459
Ritis Barauskas <perm300ru@yandex.ru>
2025-01-16 20:51:50 +0300

    added cities and places endpoints

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from events.views import index
+from events.views import index, cities, places_by_city
 
 urlpatterns = [
+    path('cities/<int:city_id>/', places_by_city, name='places_by_city'),
+    path('cities/', cities, name='cities'),
     path('', index, name='index')
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, Http404
+
+from events.constants import DATABASE
 
 
 def index(request):
     return HttpResponse('Hello, world!')
+
+
+def cities(request):
+    city_list = DATABASE.get('cities')
+
+    return render(request, 'events/cities.html', context={'cities': city_list})
+
+
+def places_by_city(request, city_id):
+    city_name = next((city.get('name') for city in DATABASE.get('cities') if city.get('id') == city_id), None)
+    if city_name is None:
+        raise Http404()
+
+    places = DATABASE.get('places')
+    filtered_places = [place for place in places if place.get('city_id') == city_id]
+
+    return render(
+        request,
+        'events/places.html',
+        {'places': filtered_places, 'city_name': city_name},
+    )
